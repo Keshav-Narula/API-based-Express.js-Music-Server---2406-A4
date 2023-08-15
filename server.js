ff2da83a3393628c66d4038b4215a34cd5dd84cf
KeshavNarula <115445686+Keshav-Narula@users.noreply.github.com>
2023-08-15 18:26:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,7 @@
-/*
-(c) 2022 Louis. D. Nel
 
-WARNING:
-NOTE: THIS CODE WILL NOT RUN UNTIL YOU
-ENTER YOUR OWN openweathermap.org APP_ID KEY
-
-NOTE: You need to install the npm modules by executing >npm install
-before running this server
-
-Simple express server re-serving data from openweathermap.org
-To test:
-http://localhost:3000
-or
-http://localhost:3000/weather?city=Ottawa
-to just set JSON response. (Note it is helpful to add a JSON formatter extension, like JSON Formatter, to your Chrome browser for viewing just JSON data.)
-*/
 const express = require('express') //express framework
 const http = require('http')
 const PORT = process.env.PORT || 3000 //allow environment variable to possible set PORT
-
-/*YOU NEED AN APP ID KEY TO RUN THIS CODE
-  GET ONE BY SIGNING UP AT openweathermap.org
-*/
-//let API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' //<== YOUR API KEY HERE
 
 const app = express()
 
